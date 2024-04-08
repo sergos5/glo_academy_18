495a7951d6a58c3b537cf2703b06eecf713f110c
Sergey <sergos5@yandex.ru>
2024-04-08 19:02:29 +0300

    изменил scrool

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -3,23 +3,13 @@ const scroll = () => {
     const scrollItem = document.querySelector('main > a');
     const menuItems = document.querySelectorAll('menu > ul > li > a');
     const serviceBlock = document.getElementById('service-block');
-    const portfolioBlock = document.getElementById('portfolio');
-    const calcBlock = document.getElementById('calc');
-    const commandBlock = document.getElementById('command');
-    const connectBlock = document.getElementById('connect');
 
-    const arrBloks = [
-        serviceBlock,
-        portfolioBlock,
-        calcBlock,
-        commandBlock,
-        connectBlock
-    ];
-
-    menuItems.forEach((menuItem, index) => {
+    menuItems.forEach((menuItem) => {
         menuItem.addEventListener('click', (event) => {
             event.preventDefault();
-            arrBloks[index].scrollIntoView({
+            const idBlock = event.target.getAttribute('href');
+            const block = document.querySelector(idBlock);
+            block.scrollIntoView({
                 behavior: "smooth"
             });
         });
