7e35e139c9ed55ab4702116dd645dd831e887522
Sergey <sergos5@yandex.ru>
2024-04-14 10:19:34 +0300

    основное задание 22

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -4,13 +4,22 @@ const menu = () => {
     const closeBtn = menu.querySelector('.close-btn');
     const menuItems = menu.querySelectorAll('ul > li > a');
 
+
     const handleMenu = () => {
         menu.classList.toggle('active-menu');
     };
 
+
     menuBtn.addEventListener('click', handleMenu);
-    closeBtn.addEventListener('click', handleMenu);
-    menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu));
+
+    menu.addEventListener('click', (e) => {
+        if (e.target.matches('ul > li > a') || e.target.classList.contains('close-btn')) {
+            handleMenu();
+        }
+
+    });
+
+
 };
 
 export default menu;
